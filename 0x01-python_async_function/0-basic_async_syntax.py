bec895d6f810b1c0512d0a47d6b04b8c6f7a8349
Diego Monroy <difemo91@gmail.com>
2020-07-22 18:30:36 -0500

    [DOCS] remove spaces

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,12 +8,10 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine that waits for a random delay and returns it
-    
     Parameters
     ----------
     max_delay : int
         waiting time
-    
     Returns
     -------
     float
