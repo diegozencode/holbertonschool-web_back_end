26386864373f96c7fcb54727e8ef09aa95a1e5fa
Diego Monroy <difemo91@gmail.com>
2020-07-22 16:55:44 -0500

    [FEAT] add concurrent coroutines logic

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+execute multiple coroutines at the same time
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int):
+    """return the delay values"""
+    my_list = []
+    for i in range(n):
+        my_list.append(await wait_random(max_delay))
+
+    return my_list
