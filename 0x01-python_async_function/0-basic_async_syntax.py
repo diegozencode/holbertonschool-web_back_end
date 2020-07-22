c0e49db7c5f42ce6cfc1d10f39e4f7b309c13a9e
Diego Monroy <difemo91@gmail.com>
2020-07-22 18:29:09 -0500

    [DOCS] add parameters and returns of method

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,18 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that waits for a random delay and returns it"""
+    """Asynchronous coroutine that waits for a random delay and returns it
+    
+    Parameters
+    ----------
+    max_delay : int
+        waiting time
+    
+    Returns
+    -------
+    float
+        the delay time
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
