558516c43e37d65c16d3a1d04755636527def2b5
Diego Monroy <difemo91@gmail.com>
2020-07-22 17:00:55 -0500

    [FEAT] add typing annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,13 @@
 execute multiple coroutines at the same time
 """
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int):
+async def wait_n(max_delay: int, n: int) -> List[float]:
     """return the delay values"""
     my_list = []
     for i in range(n):
