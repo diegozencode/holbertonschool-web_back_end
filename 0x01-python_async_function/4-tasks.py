48a7c61a17a6c2dcec62624585bf9a684b8fb5d9
Diego Monroy <difemo91@gmail.com>
2020-07-27 09:21:36 -0500

    [FEAT] add tasks 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+execute multiple coroutines at the same time
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the delay values
+    Parameters
+    ----------
+    n: int
+        number of times that wait_random spawn
+    max_delay: int
+        waiting time
+    Returns
+    -------
+    List[float]
+        all the delays in ascending order
+    """
+    my_list = []
+    for i in range(n):
+        my_list.append(await task_wait_random(max_delay))
+
+    return my_list
