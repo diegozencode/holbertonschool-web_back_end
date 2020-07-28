249a45c71a0eb94b7b01ab54bff06f4e335688a5
Diego Monroy <difemo91@gmail.com>
2020-07-28 17:35:09 -0500

    [FEAT] add run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Run time for four parallel comprehensions
+"""
+
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure time
+    """
+    s = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    total_time: float = time.perf_counter() - s
+    return total_time
