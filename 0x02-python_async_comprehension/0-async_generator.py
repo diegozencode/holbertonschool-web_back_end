ea39b0206141ff47cb499f76870815fcf9df646b
Diego Monroy <difemo91@gmail.com>
2020-07-28 15:52:19 -0500

    [FIX] unnused variable

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,6 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator:
     """Coroutine that will loop 10 times and wait 1 sec
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
