3731aaa7890de639584718a33d0f3c4eceba951d
Diego Monroy <difemo91@gmail.com>
2020-07-29 23:22:54 -0500

    [FEAT] add pagination project files

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Range of indexes
+    """
+    return (0, 7)
