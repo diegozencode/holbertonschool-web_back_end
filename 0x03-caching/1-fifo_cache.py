c34e0effa60b5a5d367495a55431ff17d8fd10d1
Diego Monroy <difemo91@gmail.com>
2020-08-04 12:57:43 -0500

    [FEAT] add caching time

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -5,20 +5,30 @@ Fifo caching
 
 
 from base_caching import BaseCaching
+from datetime import datetime
 
 
 class FIFOCache(BaseCaching):
     """First In First Out
     """
 
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+        self.cache_time = {}
+
+
     def put(self, key: str, item: str):
         """ Add an item in the cache
         """
         if (key is not None and item is not None):
+            self.cache_time[key] = datetime.now()
             self.cache_data[key] = item
             if len(self.cache_data) > self.MAX_ITEMS:
-                my_list = list(self.cache_data)
+                my_list = [k for k, v in sorted(self.cache_time.items(), key=lambda p: p[1])]
                 del self.cache_data[my_list[0]]
+                del self.cache_time[my_list[0]]
                 print("DISCARD: " + str(my_list[0]))
 
     def get(self, key: str):
