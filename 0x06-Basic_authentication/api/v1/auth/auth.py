808ef054bc03a72f352cd2210da57430167e368a
Diego Monroy <difemo91@gmail.com>
2020-08-10 22:19:16 -0500

    [FEAT] add path authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,14 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Nothing so far
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        my_list = [x[:-1] for x in excluded_paths if x[-1] == '/']
+        if path in excluded_paths or path in my_list:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Nothing so far
