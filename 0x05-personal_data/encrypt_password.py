836fda67ce3255816b0d086b84faa3d685baf347
Diego Monroy <difemo91@gmail.com>
2020-08-18 20:48:52 -0500

    [FEAT] add encoding

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -10,12 +10,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash a password
     """
-    passwd = b"{password}"
-    hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """check valid password
     """
-    return bcrypt.checkpw(b"{password}", hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
