564d57607b1fc528d1869e228ec063c5575b1ba1
Diego Monroy <difemo91@gmail.com>
2020-08-19 16:59:31 -0500

    [FEAT] add base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,19 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Auth
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """base 64
+        """
+        header = "Basic "
+        if authorization_header is None:
+            return None
+        if not type(authorization_header) == str:
+            return None
+        if len(authorization_header) < 6:
+            return None
+        if authorization_header.startswith(header):
+            return authorization_header[6:]
+        else:
+            return None
