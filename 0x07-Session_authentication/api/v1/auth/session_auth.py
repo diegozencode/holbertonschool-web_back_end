64d94003425e1443873898e14d46b9f04b86a45f
Diego Monroy <difemo91@gmail.com>
2020-08-20 15:18:06 -0500

    [FEAT] add create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,21 @@ Empty session
 
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """new authentication mechanism
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create new session
+        """
+        if user_id is None:
+            return None
+        if not type(user_id) == str:
+            return None
+        self.id = str(uuid.uuid4())
+        self.user_id_by_session_id[self.id] = user_id
+        return self.id
