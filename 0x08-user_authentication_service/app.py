6463aecd8ade67fa079153762e5c37bbdf6d9fcc
Diego Monroy <difemo91@gmail.com>
2020-08-24 13:08:03 -0500

    [FEAT] add register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,10 +4,12 @@ Flask app
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -15,6 +17,19 @@ def index() -> str:
     """welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /app/users/
+    """
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+    try:
+        AUTH.register_user(email, passwd)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
