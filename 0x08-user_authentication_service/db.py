fb22deda2cb9a963b147ed76d23abe078203b02e
Diego Monroy <difemo91@gmail.com>
2020-08-23 21:21:34 -0500

    [DOCS] add docstrings

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -9,11 +9,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base
 from user import User
+from typing import TypeVar
 
 
 class DB:
+    """database class
+    """
 
     def __init__(self):
+        """Initialize
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -21,12 +26,14 @@ class DB:
 
     @property
     def _session(self):
+        """create session
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """save the user to the database
         """
         session = self._session
