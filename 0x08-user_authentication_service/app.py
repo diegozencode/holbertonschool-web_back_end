14f41306b4d3c64e33c78e1b2656a258362cf4a0
Diego Monroy <difemo91@gmail.com>
2020-08-24 12:34:57 -0500

    [FIX] methods typo

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', method=['GET'])
+@app.route('/', methods=['GET'])
 def message() -> str:
     """welcome message
     """
