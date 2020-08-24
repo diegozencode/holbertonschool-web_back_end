d9154c24ca2932eae1738539a35bc34d607286dc
Diego Monroy <difemo91@gmail.com>
2020-08-23 21:27:31 -0500

    [FEAT] change what is print on console

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -19,7 +19,7 @@ class DB:
     def __init__(self):
         """Initialize
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,3 +41,7 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by():
+        """find user
+        """
