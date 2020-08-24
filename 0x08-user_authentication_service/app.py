29911fda5538a0606106f854cfdf2f06d1fd8aad
Diego Monroy <difemo91@gmail.com>
2020-08-24 12:29:46 -0500

    [FEAT] add basic flask app

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', method=['GET'])
+def message() -> str:
+    """welcome message
+    """
+    return jsonify({"message": "bienvenue"})
+
+
+if __name__ = "__main__":
+    app.run(host="0.0.0.0", port="5000")
