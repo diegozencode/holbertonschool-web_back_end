c44aa3499d7e764be4ff01aaf75e9b185a0b7016
Diego Monroy <difemo91@gmail.com>
2020-08-23 20:53:35 -0500

    [FEAT] add create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+DB create user
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from user import Base
+from user import User
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """save the user to the database
+        """
+        session = self._session
+        new_user = User(email=email, hashed_password=hashed_password)
+        session.add(new_user)
+        session.commit()
+        return new_user
