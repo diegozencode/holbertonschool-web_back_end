599d2b43d27799b0158e730bcf42678ffdec21b2
Diego Monroy <difemo91@gmail.com>
2020-08-24 12:20:36 -0500

    [FEAT] add register user validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,6 +5,28 @@ Hash password
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a user
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            passwd = _hash_password(password)
+            user = self._db.add_user(email, passwd)
+            return user
+        raise ValueError(f"User {email} already exists")
 
 
 def _hash_password(password: str) -> str:
