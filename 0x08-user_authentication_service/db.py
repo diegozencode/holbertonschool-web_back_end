ecf687c5544f1291c4897824f9474641aed54151
Diego Monroy <difemo91@gmail.com>
2020-08-23 21:32:50 -0500

    [FEAT] add return type

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -9,7 +9,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base
 from user import User
-from typing import TypeVar
 
 
 class DB:
@@ -33,13 +32,12 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """save the user to the database
         """
-        session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
-        session.add(new_user)
-        session.commit()
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
 
     def find_user_by():
