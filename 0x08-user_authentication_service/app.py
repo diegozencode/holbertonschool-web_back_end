e800304cd2c5fbfd1a69d5c3c96ec862b8dcc509
Diego Monroy <difemo91@gmail.com>
2020-08-24 19:56:32 -0500

    [FEAT] add get reset password token function route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -75,5 +75,19 @@ def profile() -> str:
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+    """
+    email = request.form.get('email')
+    if email:
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": token}), 200
+        except ValueError:
+            abort(403)
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
