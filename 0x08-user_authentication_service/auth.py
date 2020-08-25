dcba0b42f2e329170911ba99d707bb787728b832
Diego Monroy <difemo91@gmail.com>
2020-08-24 20:35:44 -0500

    [FIX] add correct password property

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -89,7 +89,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             passwd = _hash_password(password)
-            self._db.update_user(user.id, password=passwd, reset_token=None)
+            self._db.update_user(user.id, hashed_password=passwd, reset_token=None)
         except NoResultFound:
             raise ValueError
 
