dd44a2ba639d30856e68594b48c74506cbc680b3
Diego Monroy <difemo91@gmail.com>
2020-08-25 20:35:08 -0500

    [FEAT] add mock tests

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Test client
+"""
+
+
+import unittest
+from unittest.mock import Mock, patch
+
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test github client
+    """
+    def test_org(self):
+        """Test org url
+        """
+        pass
+
+    def test_public_repos_url(self):
+        """test public repos url
+        """
+        pass
+
+    def test_public_repos(self):
+        """test public repos
+        """
+        pass
+
+    def test_has_license(self):
+        """if has license
+        """
+        pass
+
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Test integration
+    """
+    pass
