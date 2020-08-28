c7c327adfbedbb9317596dfd14134fe2a19d9b3c
Diego Monroy <difemo91@gmail.com>
2020-08-27 21:44:07 -0500

    [FEAT] add render template

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -4,13 +4,13 @@ Flask application
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'index page'
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
