4d3142ba9e3a61018bfaf1d424ff17d32009672b
Diego Monroy <difemo91@gmail.com>
2020-08-28 12:29:49 -0500

    [FEAT] add get locale from request

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Basic babel setup
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_local():
+    """get local time
+    """
+    return request.accept_languages.best_match(app)
+
+
+@app.route('/')
+def index():
+    """call index template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
