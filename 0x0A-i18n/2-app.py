617e46647abcac021420885102177d4367cc080d
Diego Monroy <difemo91@gmail.com>
2020-08-28 19:11:13 -0500

    [FEAT] correct parametrize template

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -33,7 +33,7 @@ def get_local() -> Optional[str]:
 def index():
     """call index template
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
