67c5442fdcc6adc32cfeac7343b1ef682e0fb53a
Diego Monroy <difemo91@gmail.com>
2020-09-01 20:16:39 -0500

    [FEAT] add increment values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,6 +7,17 @@ Writing string to Redis
 import uuid
 import redis
 from typing import Union, Callable, Any
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
@@ -20,6 +31,7 @@ class Cache:
         # flush the instance with 'flushdb'
         self._redis.flushdb
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store the input
         """
