e01175c6a47c266d993145c70f9647fe9e73cbf8
Diego Monroy <difemo91@gmail.com>
2020-09-04 15:20:55 -0500

    [FEAT] add in and not out enum countries

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,7 @@
 -- create new user
-
+CREATE TABLE IF NOT EXISTS users (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL 
+    );
