2985f943ddd9ee2a02e2d0f8cf4fce714dc4eb9b
Diego Monroy <difemo91@gmail.com>
2020-09-07 20:52:57 -0500

    [FEAT] add insert a document in python

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -2,3 +2,12 @@
 """
 insert a document
 """
+
+
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    # for key, value in kwargs.items():
+    _id = mongo_collection.insert(kwargs)
+    return _id
