8b4b6c141a7590fd9780302f16575db8feba4c3a
Diego Monroy <difemo91@gmail.com>
2020-09-07 20:43:55 -0500

    [FEAT] add list all documents in python

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -2,3 +2,13 @@
 """
 List all documents
 """
+
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    documents = mongo_collection.find()
+    if documents is None:
+        return []
+    return documents
