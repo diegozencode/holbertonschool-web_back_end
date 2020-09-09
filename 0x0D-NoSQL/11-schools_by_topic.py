f12539cb4ef1bb11d5a0cd4c553a74b2eec58fbc
Diego Monroy <difemo91@gmail.com>
2020-09-08 19:54:23 -0500

    [FEAT] add list of school that have an specific topic

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -2,3 +2,13 @@
 """
 List of schools for a topic
 """
+
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ list os school having a specific topic
+    """
+    query = {"topics": topic}
+    return mongo_collection.find(query)
