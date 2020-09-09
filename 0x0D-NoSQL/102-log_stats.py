08c38916466e831ddaffdc439cd26367c8b2897a
Diego Monroy <difemo91@gmail.com>
2020-09-09 12:19:15 -0500

    [FEAT] add log stats - new version

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -2,3 +2,34 @@
 """
 Log stats - new version
 """
+
+
+from pymongo import MongoClient
+import pymongo
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    number_documents = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{number_documents} logs")
+    print("Methods:")
+    for method in methods:
+        n_docs = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {n_docs}")
+    number_docs_path = collection.count_documents({
+        "$and": [
+            {"method": "GET"},
+            {"path": "/status"}
+        ]
+    })
+    print(f"{number_docs_path} status check")
+    print("IPs:")
+    ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "myCount": {"$sum": 1}}},
+        {"$limit": 10}
+    ])
+    for x in ips:
+        print(f"\t{x['_id']}: {x['myCount']}")
