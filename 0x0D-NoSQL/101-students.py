8ad69f5a0cc1a4dea7ba474ca5beb7d2893e026f
Diego Monroy <difemo91@gmail.com>
2020-09-08 21:49:03 -0500

    [FEAT] add top students

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -2,3 +2,17 @@
 """
 Top students
 """
+
+
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """all students sorted by average score
+    """
+    query = mongo_collection.aggregate([
+        {'$project': {'name': '$name',
+                      'averageScore': {'$avg': '$topics.score'}}},
+        {'$sort': {'averageScore': -1}}
+    ])
+    return query
