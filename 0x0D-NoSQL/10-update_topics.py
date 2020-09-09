e7fca6ba937c170853a31fb64aae113c706ac269
Diego Monroy <difemo91@gmail.com>
2020-09-08 19:43:07 -0500

    [FEAT] add change school topics

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -2,3 +2,14 @@
 """
 Change school topics
 """
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """ change school topics based on name
+    """
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_one(query, new_values)
