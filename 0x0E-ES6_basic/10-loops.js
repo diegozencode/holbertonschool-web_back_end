4b3b423270eade8090ae160a02acf3fa762b9f7b
Diego Monroy <difemo91@gmail.com>
2020-09-14 22:07:46 -0500

    [FIX] correct concatenation assignment

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = array;
   for (const [key, value] of newArray.entries()) {
-    newArray[key] = `${appendString}${value}`;
+    newArray[key] = appendString + value;
   }
 
   return newArray;
