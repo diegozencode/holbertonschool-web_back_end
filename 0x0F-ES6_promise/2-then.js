53961d564f7d5973b251072ae24420df92397054
Diego Monroy <difemo91@gmail.com>
2020-09-15 23:24:27 -0500

    [FEAT] add handlers

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  })
+    .then(() => {
+      console.log('Got a response from the API');
+      return Error();
+    });
+}
