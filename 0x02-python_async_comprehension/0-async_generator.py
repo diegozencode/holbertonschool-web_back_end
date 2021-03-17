699e4431df1c8ceb5025d3f76d180b2608d7be93
Diego <difemo91@gmail.com>
2021-03-17 11:54:35 -0500

    [FEAT - ASYNC COMPREHENSION] add async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ Async generator
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine that will loop 10 times and wait 1 sec
     """
     for _ in range(10):
