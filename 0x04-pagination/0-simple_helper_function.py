0bcb18b49393e860f82c1f681ecd27792ee9b295
Diego <difemo91@gmail.com>
2021-03-23 17:40:40 +0000

    [FEAT] add simple helper pagination function

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -9,5 +9,25 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Range of indexes
+    Parameters
+    ------------
+    page: int
+        page number
+    page_size: int
+        size of the page
+    Returns
+    --------
+    tuple
+        The start and end indexes
     """
-    return (0, 7)
+    start: int
+    end: int
+
+    end = page * page_size
+
+    if page == 1:
+        start = 0
+    else:
+        start = end - page_size
+
+    return (start, end)
