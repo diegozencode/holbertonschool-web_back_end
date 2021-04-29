89c228c0b164fcb94484ed9d6720daff55ee787e
Diego <difemo91@gmail.com>
2021-04-29 02:30:57 +0000

    [FEAT - MYSQL] add bonus procedure

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,2 +1,26 @@
 -- creates a stored procedure AddBonus that adds a new correction
 
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN user_id int,
+    IN project_name varchar(255),
+    IN score int
+)
+BEGIN
+    INSERT INTO projects (name)
+    SELECT * FROM (SELECT project_name) AS tmp
+    WHERE NOT EXISTS (
+        SELECT name FROM projects WHERE name = project_name
+    ) LIMIT 1;
+    INSERT INTO corrections (
+        user_id, project_id, score
+    )
+    VALUES (
+        user_id,
+        (SELECT id FROM projects WHERE name = project_name),
+        score
+    );
+END$$
+
+DELIMITER ;
