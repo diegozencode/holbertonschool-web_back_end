3cd41c450c9ef28b9b94002f174277587446b7df
Diego <difemo91@gmail.com>
2021-04-29 20:14:30 +0000

    [FEAT - MYSQL] add safe divided function

--- 0x0C-MySQL_Advanced/10-div.sql
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -1,2 +1,19 @@
 -- safe divide
+DELIMITER $$
 
+DROP FUNCTION IF EXISTS SafeDiv $$
+
+CREATE FUNCTION SafeDiv(
+    a int,
+    b int
+)
+RETURNS float
+BEGIN
+    IF b = 0 THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END$$
+
+DELIMITER ;
