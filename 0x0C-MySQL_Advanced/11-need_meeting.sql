ffa41963de9ce61a56718796383ce28b2cbc7c6e
Diego <difemo91@gmail.com>
2021-04-29 20:33:13 +0000

    [FEAT - MYSQL] add view for meeting students

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,4 @@
 -- creates a view that list all students
-
+CREATE OR REPLACE VIEW need_meeting
+AS SELECT name FROM students
+WHERE score < 80 and (last_meeting IS NULL or last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH ));
