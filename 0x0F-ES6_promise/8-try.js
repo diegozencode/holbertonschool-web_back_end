9906eaf50e7e2c530893d965a66a5dc692a5400b
Diego <difemo91@gmail.com>
2021-05-20 21:06:46 +0000

    [FEAT - PROMISE] add error

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,7 +1,8 @@
 export default function divideFunction(numerator, denominator) {
   try {
-    if (denominator === 0)
-      throw error
+    if (denominator === 0) {
+      throw new Error();
+    }
     return numerator / denominator;
   } catch (error) {
     throw Error('cannot divide by 0');
