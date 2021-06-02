aa7814b2c6a793d2fe76e12473a1986c4fadd777
Diego <difemo91@gmail.com>
2021-06-02 02:42:31 +0000

    [FIX - ES6 CLASSES] call the method of currecy in the right order

--- 0x10-ES6_classes/4-main.js
+++ 0x10-ES6_classes/4-main.js
@@ -1,6 +1,10 @@
 import Pricing from './4-pricing.js';
 import Currency from './3-currency.js';
 
-const p = new Pricing(100, new Currency("EUR", "Euro"))
+const p = new Pricing(100, new Currency("EUR", "Euro"));
 console.log(p);
 console.log(p.displayFullPrice());
+
+const t = new Pricing(100, new Currency('$', 'Dollars'));
+console.log(t);
+console.log(t.displayFullPrice());
--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${new Currency(this._currency.name, this._currency.code).displayFullCurrency()}`;
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
