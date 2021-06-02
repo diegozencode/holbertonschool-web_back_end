cdd0e397542045c479ef367e7ce43f41a95bb31a
Diego <difemo91@gmail.com>
2021-06-02 00:30:46 +0000

    [FEAT - ES6 CLASSES] add errors if type do not match

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -22,18 +22,24 @@ export default class HolbertonCourse {
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
 
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
 
   set students(students) {
     if (Array.isArray(students)) {
       this._students = students;
+    } else {
+      throw new TypeError('Students must be an array os strings');
     }
   }
 }
