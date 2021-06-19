3b9c322b1b3748046ef4dc5970ced53c92ca0d9c
Diego <difemo91@gmail.com>
2021-06-19 15:33:29 +0000

    [FEAT - NODEJS] add a more complex HTTP server using express

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const data = await countStudents(process.argv[2]);
+  res.send(`This is the list of our student\n${data}`);
+});
+
+app.listen(1245);
+
+module.exports = app;
