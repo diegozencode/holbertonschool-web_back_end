ba0938e52e90c94f5fe8d2688bddfb6ab30bc208
Diego <difemo91@gmail.com>
2021-06-20 02:06:51 +0000

    [FEAT - UNITTESTS] add test for negative numbers

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -14,7 +14,16 @@ describe('calculateNumber', function() {
   it("checks equality", function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it("checks equality second number rounded", function() {
-    assert.strictEqual(calculateNumber(3, 3.7), 7);
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(4, 1.6), 6);
+  });
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(-1.5, 3.5), 3);
+  });
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(1.5, -3.4), -1);
+  });
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(-1.3, -3.7), -5);
   });
 });
